fb81261c8f3a7c17beab71f586e81caee5f3f172
nictrix <nickwillever@gmail.com>
2017-11-22 23:00:45 -0700

    update http prefix

--- website/config.rb
+++ website/config.rb
@@ -46,7 +46,12 @@ page '/*.txt', layout: false
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
+#set :relative_links, true
+
 configure :build do
+  set :http_prefix, '/test-githubpages/'
+
+#  activate :relative_assets
   activate :minify_css
   activate :minify_javascript
 end
